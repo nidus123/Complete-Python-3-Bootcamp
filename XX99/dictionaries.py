2dc0356a6bc243f8a0da79bbeb1126b8da98aa27
Iskren Mitkov <iskren.mitkov@gmail.com>
2023-07-02 05:00:25 +0200

    last commit before gpu gone

--- XX99/dictionaries.py
+++ XX99/dictionaries.py
@@ -1,2 +1,10 @@
 mydic = {'key1': 1, 'key2': 2, 'key3': 3, 'key4': 4, 'key5': 5}
-mydic
+mydic['key3']
+dict1 = {'key1': 1, 'key2': [1,2,3,4], 'key3': 'something', 'key4': '4th key', 'key5': '5th key' }
+dict1['key5'].upper()
+dict1['key6'] = 'number 42'
+dict1
+
+dict1.keys()
+dict1.values()
+dict1.items()
